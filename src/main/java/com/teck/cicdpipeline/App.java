e25f3424aa73d4e5529ed1e25d8c50002f7d5604
kameshsaripaka <saripaka.kamesh@gmail.com>
2026-06-03 12:00:35 +0530

    My First Commit

---
+++ src/main/java/com/teck/cicdpipeline/App.java
@@ -0,0 +1,13 @@
+package com.teck.cicdpipeline;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ src/test/java/tests/LoginTest.java
@@ -0,0 +1,29 @@
+package tests;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+import org.openqa.selenium.chrome.ChromeOptions;
+
+public class LoginTest {
+
+    @Test
+    public void verifyTitle() {
+
+    	ChromeOptions options = new ChromeOptions();
+
+        options.addArguments("--headless=new");
+        options.addArguments("--no-sandbox");
+        options.addArguments("--disable-dev-shm-usage");
+
+        WebDriver driver = new ChromeDriver(options);
+
+        driver.get("https://the-internet.herokuapp.com/login");
+
+        Assert.assertEquals(driver.getTitle(),
+                "The Internet");
+
+        driver.quit();
+    }
+}
